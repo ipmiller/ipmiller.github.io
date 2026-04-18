66303920c4558df50bc1eaf5c0ba507578c77f2f
Paul Lisker <plisker@gmail.com>
2026-04-17 23:23:56 -0500

    Fix runtime bugs: remove IE shim, correct broken image path, preconnect, and missing CSS import

--- assets/js/main.js
+++ assets/js/main.js
@@ -10,31 +10,4 @@
 			}, 100);
 		});
 
-	// Browser fixes.
-
-		// IE: Flexbox min-height bug.
-			if (browser.name == 'ie')
-				(function() {
-
-					var flexboxFixTimeoutId;
-
-					$window.on('resize.flexbox-fix', function() {
-
-						var $x = $('.fullscreen');
-
-						clearTimeout(flexboxFixTimeoutId);
-
-						flexboxFixTimeoutId = setTimeout(function() {
-
-							if ($x.prop('scrollHeight') > $window.height())
-								$x.css('height', 'auto');
-							else
-								$x.css('height', '100vh');
-
-						}, 250);
-
-					}).triggerHandler('resize.flexbox-fix');
-
-				})();
-
 })(jQuery);
